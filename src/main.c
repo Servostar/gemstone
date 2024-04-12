667f985d3069479ecd4303749d245ee317d4f295
servostar <sven.vogel123@web.de>
2024-04-12 17:29:39 +0200

    added documentation to main.c

--- src/main.c
+++ src/main.c
@@ -4,19 +4,31 @@
 
 #define LOG_LEVEL LOG_LEVEL_DEBUG
 
+/**
+ * @brief Log a debug message to inform about beginning exit procedures
+ * 
+ */
 void notify_exit(void)
 {
     DEBUG("Exiting gemstone...");
 }
 
+/**
+ * @brief Run compiler setup here
+ * 
+ */
 void setup(void)
 {
+    // setup preample
+
     log_init();
     DEBUG("starting gemstone...");
 
     #if LOG_LEVEL <= LOG_LEVEL_DEBUG
     atexit(&notify_exit);
     #endif
+
+    // actual setup
 
     DEBUG("finished starting up gemstone...");
 }
