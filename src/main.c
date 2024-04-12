4f30e6308d8cb271fda65ccc8ebbef95f14be2c4
servostar <sven.vogel123@web.de>
2024-04-12 17:28:20 +0200

    added function for notifying about exit

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
+#include <stdlib.h>
 #include <sys/log.h>
 #include <yacc/parser.tab.h>
 
 #define LOG_LEVEL LOG_LEVEL_DEBUG
 
+void notify_exit(void)
+{
+    DEBUG("Exiting gemstone...");
+}
+
 void setup(void)
 {
     log_init();
     DEBUG("starting gemstone...");
+
+    #if LOG_LEVEL <= LOG_LEVEL_DEBUG
+    atexit(&notify_exit);
+    #endif
 
     DEBUG("finished starting up gemstone...");
 }
