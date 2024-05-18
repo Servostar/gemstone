8894c6f8423a8b9e5b3c0e1e9f3dad3f6ba1acc4
servostar <sven.vogel123@web.de>
2024-05-18 12:52:00 +0200

    fixed docker builld script

--- run-docker-build.sh
+++ run-docker-build.sh
@@ -47,9 +47,9 @@ echo "+--------------------------------------+"
 echo "| RUNNING check test                   |"
 echo "+--------------------------------------+"
 
-docker run -it servostar/gemstone:devkit-"$SDK" sh run-check-test.sh
+docker run servostar/gemstone:devkit-"$SDK" sh run-check-test.sh
 if [ ! $? -eq 0 ]; then
-  echo "===> failed to build devkit"
+  echo "===> failed to run build or checks"
   exit 1
 fi
 
