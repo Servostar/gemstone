0f85dc259fe17f8253d851d7fa2d58b7464c70ba
servostar <sven.vogel123@web.de>
2024-05-21 09:56:41 +0200

    finished type test

--- tests/llvm/typedef/typedef.c
+++ tests/llvm/typedef/typedef.c
@@ -1,4 +1,6 @@
-#include "llvm/types/scope.h"
+#include "llvm/types/composite-types.h"
+#include <llvm/types/structs.h>
+#include <llvm/types/scope.h>
 #include <ast/ast.h>
 #include <stdlib.h>
 #include <sys/log.h>
@@ -8,8 +10,7 @@
 #include <llvm/backend.h>
 #include <codegen/backend.h>
 #include <llvm/types/type.h>
-
-#define LOG_LEVEL LOG_LEVEL_DEBUG
+#include <assert.h>
 
 extern FILE *yyin;
 AST_NODE_PTR root;
@@ -73,12 +74,13 @@ void run_backend_codegen() {
   err = deinit_backend();
 }
 
-void check_typedef(const AST_NODE_PTR node) {
-    TypeScopeRef scope = type_scope_new();
-
-    GemstoneTypedefRef typdef = get_type_def_from_ast(scope, node);
-
-    type_scope_delete(scope);
+void check_type(const TypeScopeRef scope, const char* name, enum Sign_t sign, enum Scale_t scale, enum Primitive_t prim) {
+  GemstoneTypedefRef type = type_scope_get_type_from_name(scope, name);
+  INFO("Expected: %d %d %d Given: %d %d %d", sign, scale, prim, type->type->specs.composite.sign, type->type->specs.composite.scale, type->type->specs.composite.prim);
+  assert(type->type->kind == TypeComposite);
+  assert(type->type->specs.composite.prim == prim);
+  assert(type->type->specs.composite.scale == scale);
+  assert(type->type->specs.composite.sign == sign);
 }
 
 int main(int argc, char *argv[]) {
@@ -105,9 +107,36 @@ int main(int argc, char *argv[]) {
   root = AST_new_node(AST_Module, NULL);
   yyparse();
 
+  TypeScopeRef scope = type_scope_new();
+
   for (size_t i = 0; i < root->child_count; i++) {
-    check_typedef(root->children[i]);
+    GemstoneTypedefRef typdef = get_type_def_from_ast(scope, root->children[i]);
+
+    type_scope_append_type(scope, typdef);
   }
+  
+  check_type(scope, "u8", Unsigned, ATOM, Int);
+  check_type(scope, "u16", Unsigned, HALF, Int);
+  check_type(scope, "u32", Unsigned, SINGLE, Int);
+  check_type(scope, "u64", Unsigned, DOUBLE, Int);
+  check_type(scope, "u128", Unsigned, QUAD, Int);
+  check_type(scope, "u256", Unsigned, OCTO, Int);
+
+  check_type(scope, "i8", Signed, ATOM, Int);
+  check_type(scope, "i16", Signed, HALF, Int);
+  check_type(scope, "i32", Signed, SINGLE, Int);
+  check_type(scope, "i64", Signed, DOUBLE, Int);
+  check_type(scope, "i128", Signed, QUAD, Int);
+  check_type(scope, "i256", Signed, OCTO, Int);
+
+  check_type(scope, "short_int", Signed, HALF, Int);
+
+  check_type(scope, "f16", Signed, HALF, Float);
+  check_type(scope, "f32", Signed, SINGLE, Float);
+  check_type(scope, "f64", Signed, DOUBLE, Float);
+  check_type(scope, "f128", Signed, QUAD, Float);
+
+  type_scope_delete(scope);
 
   AST_delete_node(root);
   return 0;
