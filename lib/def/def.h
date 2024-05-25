ed008e0c7d9e104eb64bfb67dde960d123da3a45
servostar <sven.vogel123@web.de>
2024-05-25 09:59:38 +0200

    added standard type definitions

---
+++ lib/def/def.h
@@ -0,0 +1,25 @@
+// Author: Sven Vogel
+// Edited: 25.05.2024
+// License: GPL-2.0
+
+#ifndef GEMSTONE_STD_LIB_DEF_H_
+#define GEMSTONE_STD_LIB_DEF_H_
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef float f32;
+typedef double f64;
+
+typedef u8* str;
+
+#endif // GEMSTONE_STD_LIB_DEF_H_
