dcd309c7b41115c8044576ffaabae027b8146238
servostar <sven.vogel123@web.de>
2024-05-26 17:11:43 +0200

    added module type

--- src/set/types.h
+++ src/set/types.h
@@ -469,4 +469,17 @@ typedef struct Statement_t {
     } impl;
 } Statement;
 
+// .------------------------------------------------.
+// |                   Module                       |
+// '------------------------------------------------'
+
+typedef struct Module_t {
+    GHashTable* boxes;
+    GHashTable* types;
+    GHashTable* functions;
+    GHashTable* variables;
+    // to be resolved after the module has been parsed completely
+    GArray* imports;
+} Module;
+
 #endif // SET_TYPES_H_
