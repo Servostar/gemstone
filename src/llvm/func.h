28a4f619a0f035164ed27acc4e2ae47ec5cc54b9
servostar <sven.vogel123@web.de>
2024-05-28 22:10:04 +0200

    fixed: added missing header func.h

---
+++ src/llvm/func.h
@@ -0,0 +1,27 @@
+
+#ifndef LLVM_BACKEND_FUNC_H_
+#define LLVM_BACKEND_FUNC_H_
+
+#include <llvm/parser.h>
+
+#include <glib.h>
+
+typedef struct LLVMFuncScope_t {
+    LLVMGlobalScope* global_scope;
+    // of LLVMTypeRef
+    GHashTable* params;
+    LLVMValueRef llvm_func;
+} LLVMFuncScope;
+
+typedef struct LLVMLocalScope_t LLVMLocalScope;
+
+typedef struct LLVMLocalScope_t {
+    // of LLVMTypeRef
+    GHashTable* vars;
+    LLVMFuncScope* func_scope;
+    LLVMLocalScope* parent_scope;
+} LLVMLocalScope;
+
+LLVMValueRef get_variable(const LLVMLocalScope* scope, const char* name);
+
+#endif // LLVM_BACKEND_FUNC_H_
